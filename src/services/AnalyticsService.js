6d75c637e2033c7964dd346de1983d6b3c11082c
Victor Vidal <victor_vdu@hotmail.com>
2024-07-14 22:29:24 -0600

    Analytics service improved, working

--- src/services/AnalyticsService.js
+++ src/services/AnalyticsService.js
@@ -19,7 +19,6 @@ export const AnalyticsService = {
     }
   },
 
-  // Ejemplos de eventos específicos
   logScreenView: async (screenName) => {
     await AnalyticsService.logEvent('screen_view', { screen_name: screenName });
   },
@@ -32,7 +31,41 @@ export const AnalyticsService = {
     await AnalyticsService.logEvent('plan_started', { plan_id: planId });
   },
 
-  logSearch: async (query) => {
-    await AnalyticsService.logEvent('search', { query });
+  logSearch: async (query, searchType, resultsCount) => {
+    await AnalyticsService.logEvent('search', { query, search_type: searchType, results_count: resultsCount });
+  },
+
+  logVerseRead: async (book, chapter, verse) => {
+    await AnalyticsService.logEvent('verse_read', { book, chapter, verse });
+  },
+
+  logBookmarkAdded: async (book, chapter, verse) => {
+    await AnalyticsService.logEvent('bookmark_added', { book, chapter, verse });
+  },
+
+  logNoteAdded: async (book, chapter, verse) => {
+    await AnalyticsService.logEvent('note_added', { book, chapter, verse });
+  },
+
+  logShareVerse: async (book, chapter, verse) => {
+    await AnalyticsService.logEvent('verse_shared', { book, chapter, verse });
+  },
+
+  logReadingPlanProgress: async (planId, dayCompleted) => {
+    await AnalyticsService.logEvent('reading_plan_progress', { plan_id: planId, day_completed: dayCompleted });
+  },
+
+  logSettingsChanged: async (settingName, newValue) => {
+    await AnalyticsService.logEvent('settings_changed', { setting_name: settingName, new_value: newValue });
+  },
+
+  logError: async (errorType, errorMessage) => {
+    await AnalyticsService.logEvent('app_error', { error_type: errorType, error_message: errorMessage });
+  },
+
+  logPerformanceMetric: async (metricName, value) => {
+    await AnalyticsService.logEvent('performance_metric', { metric_name: metricName, value });
   },
 };
+
+export default AnalyticsService;
